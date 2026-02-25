546b98f51540f0b16a4c34ff5d86d306899cbae1
Nicoly <nicoly_sousa-silva@estudante.sesisenai.org.br>
2026-02-25 10:15:33 -0300

    organizar estrutura repo

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const app = express()
+
+
+app.get('/usuarios', (req, res) => {
+     res.send('Ok, deu bom')
+})
+
+app.listen(3000, () => {
+     console.log('Servidor em Execução');
+})
+
+
+
