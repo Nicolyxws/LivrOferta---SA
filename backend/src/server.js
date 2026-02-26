a97b6daffb38807a5bffeca4ee2fe5ced2accfa0
Nicoly <nicoly_sousa-silva@estudante.sesisenai.org.br>
2026-02-26 08:44:25 -0300

    Atualização do código

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,9 +2,19 @@ import express from 'express'
 
 const app = express()
 
+app.use(express.json())
+
+const users = []
+
+app.post('/usuarios', (req, res) => {
+     
+     users.push(req.body)
+     
+     res.send('Ok, post')
+})
 
 app.get('/usuarios', (req, res) => {
-     res.send('Ok, deu bom')
+     res.json(users)
 })
 
 app.listen(3000, () => {
