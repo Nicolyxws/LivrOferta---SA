f60aa91659f274c228a66060c76c679ff2a81241
Nicoly <nicoly_sousa-silva@estudante.sesisenai.org.br>
2026-03-04 10:53:37 -0300

    nova atualização

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,25 +1,41 @@
-import express from 'express'
+require('dotenv').config();
+const express = require("express");
+const mysql = require('mysql2');  
 
-const app = express()
 
-app.use(express.json())
+const app = require("./app");
+const PORT = process.env.PORT || 3000;
 
-const users = []
+//CONEXÃO COM MYSQL 
+const db = mysql.createConnection ({
+    host: process.env.DB_HOST ||'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'senai',
+    database: process.env.DB_NAME ||'livroferta'
+});
+
+
+app.use(express.json());
 
 app.post('/usuarios', (req, res) => {
-     
-     users.push(req.body)
-     
-     res.send('Ok, post')
-})
+    const {nome, email, senha} = req.body
+ 
+    const sql = "INSERT INTO usuarios (nome, email, senha) values(?, ?, ?)";
 
+
+    db.query(sql, [nome, email, senha], (err, result) => {
+     if(err) {
+          return res.status(500).json({error: err.message});
+     }
+    res.status(201).json({mesage: "Usuário criado.", id:result.insertId});    
+});
+});
 app.get('/usuarios', (req, res) => {
      res.json(users)
+});
+
+
+
+app.listen(PORT, () => {
+ console.log(`Servidor rodando na porta ${PORT}`);
 })
-
-app.listen(3000, () => {
-     console.log('Servidor em Execução');
-})
-
-
-
