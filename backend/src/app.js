65fab46aced3abce344c72ca0747f055fbf46488
joaolawall21 <joaopedrolawall21@gmail.com>
2026-03-11 11:13:52 -0300

    Add files via upload

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,13 @@
+require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+
+const livroRoutes = require("./routes/livros.routes");
+
 const app = express();
+app.use(cors());
+app.use(express.json());
 
+app.use("/livros", livroRoutes.router);
 
-
-module.exports = app; 
+module.exports =  app;
--- backend/src/routes/livros.routes.js
+++ backend/src/routes/livros.routes.js
@@ -1,2 +1,15 @@
 const express = require("express")
-cosnt
+const livrosController = require("../controller/livros.controller")
+const router = express.Router()
+
+router.get("/", livrosController.ListarLivros)
+
+router.post("/", livrosController.CriarLivros)
+
+// router.get("/:id", livrosController.DetalharLivro)
+
+router.delete("/:id", livrosController.DeleteLivro)
+
+// router.put("/:id", livrosController.atualizarLivros)
+
+module.exports = { router }
