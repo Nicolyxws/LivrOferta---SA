d320aad3f42df8640ac47ef7682f03b24d9b2e89
joaolawall21 <joaopedrolawall21@gmail.com>
2026-03-16 11:22:27 -0300

    Add files via upload

--- backend/src/routes/livros.routes.js
+++ backend/src/routes/livros.routes.js
@@ -1,15 +1,2 @@
 const express = require("express")
-const livrosController = require("../controller/livros.controller")
-const router = express.Router()
-
-router.get("/", livrosController.ListarLivros)
-
-router.post("/", livrosController.CriarLivros)
-
-// router.get("/:id", livrosController.DetalharLivro)
-
-router.delete("/:id", livrosController.DeleteLivro)
-
-// router.put("/:id", livrosController.atualizarLivros)
-
-module.exports = { router }
+cosnt
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,41 @@
-const app = require("./app");
+require('dotenv').config();
+const express = require("express");
+const mysql = require('mysql2');  
 
-const PORT =  3001;
-app.listen(PORT, () => {
-  console.log(`API rodando em http://localhost:${PORT}`);
+
+const app = require("./app");
+const PORT = process.env.PORT || 3000;
+
+//CONEXÃO COM MYSQL 
+const db = mysql.createConnection ({
+    host: process.env.DB_HOST ||'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'senai',
+    database: process.env.DB_NAME ||'livroferta'
 });
+
+
+app.use(express.json());
+
+app.post('/usuarios', (req, res) => {
+    const {nome, email, senha} = req.body
+ 
+    const sql = "INSERT INTO usuarios (nome, email, senha) values(?, ?, ?)";
+
+
+    db.query(sql, [nome, email, senha], (err, result) => {
+     if(err) {
+          return res.status(500).json({error: err.message});
+     }
+    res.status(201).json({mesage: "Usuário criado.", id:result.insertId});    
+});
+});
+app.get('/usuarios', (req, res) => {
+     res.json(users)
+});
+
+
+
+app.listen(PORT, () => {
+ console.log(`Servidor rodando na porta ${PORT}`);
+})
