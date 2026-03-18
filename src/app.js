f82b54e07aa3c720bad5171c10a8f8e1f6d2bcb1
joaolawall21 <joaopedrolawall21@gmail.com>
2026-03-18 10:21:38 -0300

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,20 +1,37 @@
+require('dotenv').config();
 const express = require("express");
-
-const cors = require("cors");
-
-require("dotenv").config();
-
-require("dotenv").config();
-
-const livrosRoutes = require("./routes/livros.routes.js");
-
-const errorMiddleware = require("./middlewares/error.middleware");
+const mysql = require('mysql2');  
 const app = express();
-app.use(cors());
+
+//TO-DO
+// rotas, regras de negocio (controllers) e models (repositories) devem ser modularizados e separados nos respectivos arquivos
+app.post('/usuarios', (req, res) => {
+    const {nome, email, senha} = req.body
+ 
+    const sql = "INSERT INTO usuarios (nome, email, senha) values(?, ?, ?)";
+
+
+    db.query(sql, [nome, email, senha], (err, result) => {
+     if(err) {
+          return res.status(500).json({error: err.message});
+     }
+    res.status(201).json({mesage: "Usuário criado.", id:result.insertId});    
+});
+});
+app.get('/usuarios', (req, res) => {
+     res.json(users)
+});
+//FIM TO-DO
+
+
+//Código organizado
 app.use(express.json());
 
-app.use( ".//livros",  livrosRoutes)
+//importar rotas
+const livroRoutes = require("../src/routes/livros.routes");
+app.use("/livros", livroRoutes);
 
+const livroPedidos = require("../src/routes/itens-pedidos.route");
+app.use("/itens-pedidos", livroPedidos);
 
-app.use(errorMiddleware);
 module.exports = app; 
